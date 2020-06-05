bbff25d7e0a88c90090fde8092ed2698cf43e40d
test <test@maonmail.fr>
2020-06-05 06:48:32 -0400

    test trigger v.01

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
  public static void main(String[] args) {
    System.out.println("Hello, World !") ;
-   System.out.println("Test trigger Git 123");
+   System.out.println("Test trigger Git v0.1");
    }
 }
